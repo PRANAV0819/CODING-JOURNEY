1d8db86c215f426ea629aae37ae5e3406c9ee5dc
PRANAV0819 <pranavwaghmare08192006@gmail.com>
2024-10-19 12:29:31 +0530

    taking two no. as input and add them

---
+++ code3.c
@@ -0,0 +1,16 @@
+// take input as two numbers ,add them
+
+
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("Enter Two numbers :");
+    scanf("%d %d",&a ,&b);
+
+    int c=a+b;
+    printf("The Sum is : %d",c=a+b);
+
+    return 0;
+
+}  
